8e9ddaadc4d00b99abdb447f3b4027bbb5502edc
Danny Steenman <15192660+dannysteenman@users.noreply.github.com>
2026-08-30 09:46:40 +0200

    chore: update cloudburn to 0.11.2

--- Formula/cloudburn.rb
+++ Formula/cloudburn.rb
@@ -1,8 +1,8 @@
 class Cloudburn < Formula
   desc "CLI for cloud cost optimization"
   homepage "https://cloudburn.io/docs"
-  url "https://registry.npmjs.org/cloudburn/-/cloudburn-0.11.1.tgz"
-  sha256 "d85b4bdad22699a021800c659a1465fd012d0029525df16cb163e36b0d4a887f"
+  url "https://registry.npmjs.org/cloudburn/-/cloudburn-0.11.2.tgz"
+  sha256 "86f256867e0845d6974e91dc2cef3f429faa53bd4429d93d2c4bb0bd0746adc3"
   license "Apache-2.0"
 
   depends_on "node"
